537c1349539c2171dd761d18314a596a1fdf8452
mayachacha <asakawa926@gmail.com>
2023-07-20 16:42:53 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,14 @@ def
   new
   @room = Room.new
 end
+
+def create
+  @room = Room.new(room_params)
+  if @room.save
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
 end
